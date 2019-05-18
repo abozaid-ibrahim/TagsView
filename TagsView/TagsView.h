7d2b180f5f6cc94dd8de5b8679fb38fa7797fa9d
abuzeid ibrahim <abuzeid7@gmail.com>
2019-05-19 00:50:15 +0200

    init tags

---
+++ TagsView/TagsView.h
@@ -0,0 +1,19 @@
+//
+//  TagsView.h
+//  TagsView
+//
+//  Created by abuzeid on 5/18/19.
+//  Copyright © 2019 abuzeid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TagsView.
+FOUNDATION_EXPORT double TagsViewVersionNumber;
+
+//! Project version string for TagsView.
+FOUNDATION_EXPORT const unsigned char TagsViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagsView/PublicHeader.h>
+
+
